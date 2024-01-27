f49b2ad81f490946fef48dc03ab7bda2517834cb
Oleg <slepcov_oleg@mail.ru>
2024-01-27 21:46:40 +0700

    Replaced jquery on javascript

--- leadapp/views.py
+++ leadapp/views.py
@@ -2,6 +2,7 @@ from rest_framework import views
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.shortcuts import render
 import re
+import json
 
 
 class InstallApiView(views.APIView):
@@ -16,7 +17,7 @@ class IndexApiView(views.APIView):
         # r = request.data.get("PLACEMENT_OPTIONS", [])
         # id_deal = re.search(r'\d+', r)[0]
         # data = {"id": id_deal, }
-        data = {"id": request.data, }
+        data = {"id": json.dumps(request.data) }
         return render(request, 'lead/index.html', context=data)
 
     @xframe_options_exempt
