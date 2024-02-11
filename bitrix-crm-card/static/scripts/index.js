f9705fc9fe6acd6879c9f4be5025026dbefe8c0d
Oleg <slepcov_oleg@mail.ru>
2024-02-11 10:21:04 +0700

    Fixed render photos

--- bitrix-crm-card/static/scripts/index.js
+++ bitrix-crm-card/static/scripts/index.js
@@ -5,6 +5,7 @@ const USERS_ACCESS = ['11789', '1'];
 
 class App {
     constructor(leadId, bx24) {
+        this.smartNumber = 172;
         this.leadId = leadId;
         this.bx24 = bx24;
         this.currentUserId = null;
@@ -13,18 +14,56 @@ class App {
 
     async init() {
         const currentUser = await this.bx24.user.getCurrent();
-        console.log("currentUser = ", currentUser);
+        // console.log("currentUser = ", currentUser);
         this.currentUserId = currentUser?.ID;
         const isAccess = this.restrictUserAccess();
         if (!isAccess) {
             this.secureData();
             return;
         }
-
-        const data = await this.bx24.batch.getDataForStart(this.leadId);
-        console.log("data = ", data);
+        const data = await this.getDataFromBx24();
+        // const data = await this.bx24.batch.getDataForStart(this.leadId);
+        // console.log("data = ", data);
 
         this.initHandler();
+    }
+
+    async getDataFromBx24() {
+        const data = await this.bx24.batch.getData({
+            // currentUser: 'user.current',
+            // departments: 'department.get',
+            fieldsLeadData: 'crm.lead.fields',
+            leadData: `crm.lead.get?id=${leadId}`,
+            fieldsProductData: `crm.item.fields?entityTypeId=${this.smartNumber}`,
+            productsData: `crm.item.list?entityTypeId=${this.smartNumber}`,
+            responsible: `user.get?id=$result[leadData][ASSIGNED_BY_ID]`,
+
+            // user: 'user.current',
+            // smartProcess: `crm.item.get?entityTypeId=${this.smartId}&id=${this.entityId}`,
+            // smartFabricList: `crm.item.list?entityTypeId=${this.smartFabricsId}&select[]=id&select[]=title&select[]=${FIELD_FABRIC.name}&select[]=${FIELD_FABRIC.image}&select[]=${FIELD_FABRIC.type}&select[]=${FIELD_FABRIC.color}&order[id]=ASC`,
+            // smartFabric: `crm.item.get?entityTypeId=${this.smartFabricsId}&id=$result[smartProcess][item][${FIELD_MSP.upholsteryFabricCollection}]&select[]=id&select[]=title&select[]=ufCrm17_1705390343&select[]=ufCrm17_1705390515&select[]=ufCrm17_1705828938`,
+            // smartFabric_1: `crm.item.get?entityTypeId=${this.smartFabricsId}&id=$result[smartProcess][item][${FIELD_MSP.upholsteryFabricCollection_1}]&select[]=id&select[]=title&select[]=ufCrm17_1705390343&select[]=ufCrm17_1705390515&select[]=ufCrm17_1705828938`,
+            // smartFabric_2: `crm.item.get?entityTypeId=${this.smartFabricsId}&id=$result[smartProcess][item][${FIELD_MSP.upholsteryFabricCollection_2}]&select[]=id&select[]=title&select[]=ufCrm17_1705390343&select[]=ufCrm17_1705390515&select[]=ufCrm17_1705828938`,
+            // fields: `crm.item.fields?entityTypeId=${this.smartId}`,
+            // createdBy: `user.get?id=$result[smartProcess][item][${FIELD_MSP.createdBy}]`,
+            // updatedBy: `user.get?id=$result[smartProcess][item][${FIELD_MSP.createdBy}]`,
+        });
+
+        console.log("data = ", data);
+
+        // this.user = data?.result?.user;
+        // this.createdUser = data?.result?.createdBy[0];
+        // this.updatedUser = data?.result?.updatedBy[0];
+        // this.data = data?.result?.smartProcess?.item;
+        // this.smartFabricList = data?.result?.smartFabricList?.items;
+        // const total = data?.result_total?.smartFabricList;
+        // let allFabrics = await this.getAllFabrics(total);
+        // this.smartFabricList = this.smartFabricList.concat(allFabrics);
+        // this.smartFabric = data?.result?.smartFabric?.item;
+        // this.smartFabric_1 = data?.result?.smartFabric_1?.item;
+        // this.smartFabric_2 = data?.result?.smartFabric_2?.item;
+        // this.fields = data?.result?.fields?.fields;
+
     }
 
     initHandler() {
